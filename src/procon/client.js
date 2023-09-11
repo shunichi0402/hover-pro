a464303c581b7fff69bf04d292b4b6e7d4d71758
shunichi0402 <49316097+shunichi0402@users.noreply.github.com>
2023-09-11 16:27:13 +0900

    [update] 左右制御の書き足し

--- src/procon/client.js
+++ src/procon/client.js
@@ -16,10 +16,53 @@ async function init(){
     return
 }
 
+function clanp(x){
+    if (x > 1){
+        return 1;
+    }
+
+    if(x < -1){
+        return -1;
+    }
+
+    return x;
+}
+
 async function outGPIO(pitch, yaw, hover, on, counter){
     if(on){
         console.log('hover')
         gpio.write(8, hover);
+
+        const right = clanp(pitch * 0.7 + yaw * 0.7);
+        const left = clanp(pitch * 0.7 + yaw * (-0.7));
+
+        if (parseInt(right * 10) > counter) {
+            gpio.write(11, true);
+            gpio.write(12, false);
+            print('right on');
+        } else if (parseInt(right * (-10)) > counter) {
+            gpio.write(11, false);
+            gpio.write(12, true);
+            print('right revarse');
+        } else {
+            gpio.write(12, false);
+            gpio.write(12, false);
+            print('right off');
+        }
+
+        if (parseInt(left * 10) > counter) {
+            gpio.write(15, true);
+            gpio.write(16, false);
+            print('left on');
+        } else if (parseInt(left * (-10)) > counter) {
+            gpio.write(15, false);
+            gpio.write(16, true);
+            print('left revarse');
+        } else {
+            gpio.write(15, false);
+            gpio.write(16, false);
+            print('left off');
+        }
     } else {
         console.log('down')
         gpio.write(8, false);
